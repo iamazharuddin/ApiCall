8cdc0c775016008b1539cfab61aed81e15609bf7
Azharuddin <iamazharuddin10@gmail.com>
2023-05-11 21:25:46 +0530

    Initial Commit

---
+++ GetApiCall/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GetApiCall
+//
+//  Created by Azharuddin 1 on 11/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GetApiCall/GetApiCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetApiCallApp.swift
+//  GetApiCall
+//
+//  Created by Azharuddin 1 on 11/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GetApiCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
